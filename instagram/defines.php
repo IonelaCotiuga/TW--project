724729ad79c4e3529a97508c845646d341b80eae
Vanessa Hoamea <vanessahoamea@yahoo.com>
2022-04-16 14:34:45 +0300

    editing page

--- instagram/defines.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('INSTAGRAM_APP_ID', '551755046197642');
-define('INSTAGRAM_APP_SECRET', '256ef79a3e21e8e39db15b38593da6b3');
-define('INSTAGRAM_APP_REDIRECT_URI', 'https://localhost/TW-Project/instagram/');
- ?>
