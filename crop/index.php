885e6b7fb87dfc5acef434ef89145435258b495a
Vanessa Hoamea <vanessahoamea@yahoo.com>
2022-05-24 09:05:44 +0300

    crop

---
+++ crop/index.php
@@ -0,0 +1,55 @@
+<?php
+  //if the user is not logged in, they will not have access to the profile page
+  session_start();
+  // if(!isset($_SESSION["username"]))
+  //   header("location: ../index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>M-Pic</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+  <body>
+    <!-- header -->
+    <nav class="topnav" id="myTopnav">
+      <a class="logo">M-PIC</a>
+      <a href="../">Home</a>
+      <a href="../about">About</a>
+      <a href="../contact">Contact us</a>
+      <a href="../profile">Profile</a>
+
+      <a href="javascript:void(0);" class="icon" onclick="expand()">
+          <i class="fa fa-bars"></i>
+      </a>
+    </nav>
+
+    <div class="wrapper">
+      <!-- the image -->
+      <div class="result">
+        <img id="chosen-image" src="cat.png" alt="The chosen image.">
+        <canvas id="canvas"></canvas>
+      </div>
+
+      <div class="editor">
+        <button class="button" onclick="reset()">Reset Selection</button>
+        <button class="button" onclick="crop()">Crop Selection</button>
+        <button class="button" onclick="save()">Save Image</button>
+      </div>
+    </div>
+
+    <canvas id="canvas2" style="display: none;"></canvas>
+
+    <!-- script -->
+    <script src="script.js"></script>
+    <script src="../script.js"></script>
+    <!-- fontawesome icons -->
+    <script src="https://kit.fontawesome.com/a4f543b8bc.js" crossorigin="anonymous"></script>
+  </body>
+
+</html>
---
+++ crop/script.js
@@ -0,0 +1,126 @@
+const image = document.getElementById("chosen-image");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//draw the image on the canvas
+canvas.width = image.width;
+canvas.height = image.height;
+ctx.drawImage(image, 0, 0, image.width, image.height);
+
+image.style.display = "none";
+
+//copy contents for creating the pattern
+const newCanvas = document.getElementById('canvas2');
+const newCtx = newCanvas.getContext("2d");
+newCanvas.width = canvas.width;
+newCanvas.height = canvas.height;
+newCtx.drawImage(canvas, 0, 0);
+
+//get mouse coordinates
+let coordinates = [];
+let saveCoordinates = [];
+canvas.onclick = function clickEvent(e)
+{
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+
+  let point = {
+    "x": x,
+    "y": y
+  };
+  coordinates.push(point);
+
+  ctx.fillStyle = "blue";
+  ctx.fillRect(x, y, 5, 5);
+}
+
+//crop the image
+function crop()
+{
+  ctx.beginPath();
+
+  //create the clipping path
+  ctx.moveTo(coordinates[0].x, coordinates[0].y);
+  saveCoordinates.push(coordinates[0]);
+  for(let i=1; i<coordinates.length; i++)
+  {
+    saveCoordinates.push(coordinates[i]);
+    ctx.lineTo(coordinates[i].x, coordinates[i].y);
+  }
+  ctx.lineTo(coordinates[0].x, coordinates[0].y);
+  ctx.stroke();
+
+  //make the rest of the canvas transparent
+  ctx.save();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+
+  //crop selection
+  ctx.clip();
+
+  //redraw the image in the cropped area
+  let pattern = ctx.createPattern(newCanvas, "repeat");
+  ctx.fillStyle = pattern;
+  ctx.fill();
+
+  ctx.closePath();
+
+  coordinates = [];
+}
+
+function save()
+{
+  //reset the auxiliary canvas
+  newCtx.clearRect(0, 0, newCanvas.width, newCanvas.height);
+
+  if(saveCoordinates.length > 0)
+  {
+    //get new dimensions
+    let top = canvas.height;
+    let left = canvas.width;
+    let right = -canvas.width;
+    let bottom = -canvas.height;
+    for(let i=0; i<saveCoordinates.length; i++)
+    {
+      if(saveCoordinates[i].y < top)
+        top = saveCoordinates[i].y;
+      if(saveCoordinates[i].x < left)
+        left = saveCoordinates[i].x;
+      if(saveCoordinates[i].x > right)
+        right = saveCoordinates[i].x;
+      if(saveCoordinates[i].y > bottom)
+        bottom = saveCoordinates[i].y;
+    }
+
+    newCanvas.width = right-left;
+    newCanvas.height = bottom-top;
+
+    //save the new image
+    newCtx.drawImage(canvas, left, top, newCanvas.width, newCanvas.height, 0, 0, newCanvas.width, newCanvas.height);
+  }
+  else
+  {
+    newCanvas.width = canvas.width;
+    newCanvas.height = canvas.height;
+    newCtx.drawImage(canvas, 0, 0);
+  }
+
+  let downloadLink = document.createElement("a");
+  downloadLink.download = "MPic_Sticker.png";
+  downloadLink.href = newCanvas.toDataURL("image/png");
+  downloadLink.click();
+}
+
+//reset selection
+function reset()
+{
+  image.style.display = "block";
+  canvas.width = image.width;
+  canvas.height = image.height;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+  image.style.display = "none";
+  coordinates = [];
+  saveCoordinates = [];
+}
